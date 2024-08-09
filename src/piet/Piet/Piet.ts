7c38ff6def7f664c383c6bc6bd5460f4360d258b
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2024-08-10 01:46:27 +0200

    feat: should input a character then output the same character

--- src/piet/Piet/Piet.ts
+++ src/piet/Piet/Piet.ts
@@ -8,11 +8,27 @@ export class Piet {
     }
 
 
-    inputNumber(number: number) {
+    public inputNumber(number: number) {
         this.stack.push(number);
     }
 
-    outputNumber(): number {
+    public outputNumber(): number {
         return this.stack.pop();
+    }
+
+    public inputCharacter(character: string) {
+        this.stack.push(this.charCodeFrom(character));
+    }
+
+    public outputCharacter(): string {
+        return this.characterFrom(this.stack.pop());
+    }
+
+    private charCodeFrom(character: string) {
+        return character.charCodeAt(0);
+    }
+
+    private characterFrom(charCode: number) {
+        return String.fromCharCode(charCode);
     }
 }
--- src/piet/Piet/__tests__/Piet.test.ts
+++ src/piet/Piet/__tests__/Piet.test.ts
@@ -11,4 +11,14 @@ describe("Piet", () => {
 
         expect(piet.outputNumber()).toBe(number);
     })
+
+    it("should input a character then output the same character", () => {
+        const stack = new Stack<number>(10);
+        const piet = new Piet(stack);
+        const character = "a";
+
+        piet.inputCharacter(character);
+
+        expect(piet.outputCharacter()).toBe(character);
+    })
 })
